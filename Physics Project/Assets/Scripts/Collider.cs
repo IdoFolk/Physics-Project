82bd4f36884198609ed42704aa85e8577e913c3d
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-02-19 16:30:31 +0200

    .

--- Physics Project/Assets/Scripts/Collider.cs
+++ Physics Project/Assets/Scripts/Collider.cs
@@ -1,12 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VInspector;
 
-public class Collider : MonoBehaviour
+[Serializable]
+public class Collider
 {
-    [SerializeField] private ColliderType colliderType;
+    public event Action<Collider> OnCollisionEnter;
+    public event Action<Collider> OnCollisionExit;
+
+    public ColliderType ColliderType { get; }
+    public Vector3 Size { get; }
+    public float Radius { get; }
+
+    public Collider(ColliderConfig config)
+    {
+        Size = config.Size;
+        ColliderType = config.ColliderType;
+        Radius = config.Radius;
+    }
+
+    public bool CheckCollision()
+    {
+        return false;
+    }
+    
 }
 
+[Serializable]
+public struct ColliderConfig
+{
+    public ColliderType ColliderType;
+    [ShowIf(nameof(ColliderType),ColliderType.Box)]public Vector3 Size;
+    [ShowIf(nameof(ColliderType),ColliderType.Sphere)]public float Radius;
+    
+    
+}
 public enum ColliderType
 {
     Box,
--- Physics Project/Assets/Scripts/PhysicsObject.cs
+++ Physics Project/Assets/Scripts/PhysicsObject.cs
@@ -2,20 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VInspector;
 
 public class PhysicsObject : MonoBehaviour
 {
-    [SerializeField] private float mass;
-    [SerializeField] private bool usingGravity;
-    [SerializeField] private float speedCheckTime;
-    public float Mass => mass;
+    public float Mass => _mass;
+
+    private float _test;
+    public float Test
+    {
+        get => _test;
+        set => _test = value;
+    }
     public float Speed => _speed;
     public Vector3 Position => transform.position;
+    [SerializeField] private float _mass;
+    [SerializeField] private bool _usingGravity;
+    [SerializeField] private bool _hasCollider;
+    [SerializeField][ShowIf(nameof(_hasCollider))] private ColliderConfig _colliderConfig;
+    
 
     private bool _isColliding;
     protected List<Force> _forces = new List<Force>();
     protected Force _gravityForce = new Force(Vector3.zero);
     private Force _angularForce = new Force(Vector3.zero);
+    private Collider _collider;
     private float _speed;
 
 
@@ -34,38 +45,43 @@ public class PhysicsObject : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        if (_hasCollider) _collider = new Collider(_colliderConfig);
+    }
+
     public virtual void Start()
     {
-        if (usingGravity) _forces.Add(_gravityForce);
-        if (usingGravity) _forces.Add(_angularForce);
+        if (_usingGravity) _forces.Add(_gravityForce);
+        if (_usingGravity) _forces.Add(_angularForce);
     }
 
 
     private void FixedUpdate()
     {
         transform.position += (Velocity) * Time.fixedDeltaTime;
-        CalculateSpeed();
+        //CalculateSpeed();
     }
 
     public void ApplyGravityForce(Vector3 force)
     {
-        if (!usingGravity) return;
+        if (!_usingGravity) return;
         if (_isColliding)
         {
             _gravityForce.Value = Vector3.zero;
             return;
         }
-        _gravityForce.Value = force / mass;
+        _gravityForce.Value = force / _mass;
         
     }
     public void ApplyAngularForce(float radius, Vector3 direction)
     {
         if(_isColliding) return;
-        var force = (Speed * Speed * mass / radius) * direction;
-        _angularForce.Value += force / mass;
+        var force = (Speed * Speed * _mass / radius) * direction;
+        _angularForce.Value += force / _mass;
     }
 
-    public void CalculateSpeed()
+    private void CalculateSpeed()
     {
         var distance = Vector3.Distance(_lastPos, Position);
         _speed = distance;
@@ -73,14 +89,20 @@ public class PhysicsObject : MonoBehaviour
         _lastPos = Position;
     }
 
-    private void OnTriggerEnter(UnityEngine.Collider other)
+    private void OnDrawGizmosSelected()
     {
-        _isColliding = true;
-    }
-
-    private void OnTriggerExit(UnityEngine.Collider other)
-    {
-        _isColliding = false;
+        if (_hasCollider)
+        {
+            switch (_colliderConfig.ColliderType)
+            {
+                case ColliderType.Box:
+                    Gizmos.DrawCube(transform.position,_colliderConfig.Size);
+                    break;
+                case ColliderType.Sphere:
+                    Gizmos.DrawSphere(transform.position,_colliderConfig.Radius);
+                    break;
+            }
+        }
     }
 }
 
